343eff86b25df27a49951561c3ae86ef393218df
bakai209 <ss4.sell@gmail.com>
2022-05-23 17:22:36 -0500

    se abandona el proyecto ya que la version 18 de react no funciono

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
+import Header from "./components/Header";
 import NavBar from "./components/NavBar";
+import AgentsContainer from "./containers/AgentsContainer";
+import CardContainer from "./containers/CardContainer";
+import InfoContainer from "./containers/InfoContainer";
 
 function App() {
   return (
-
-    <NavBar />
+    <>
     
+    <NavBar />
+    <Header />
+    <div className="home__container">
+    <InfoContainer />
+    <CardContainer />
+    <AgentsContainer />
+    </div>
+
+
+    </>
   )
 }
 
